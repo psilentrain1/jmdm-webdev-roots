81e63d0d23bb05e63eec0bc27d156debd4f35e5e
James Drake <info@jamesmdrakemedia.com>
2025-03-27 00:42:19 -0400

    feat: header scroll

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const header = document.querySelector("header");
+const logo = document.getElementById("logo");
+
+window.addEventListener("scroll", () => {
+  if (scrollY === 0) {
+    header?.classList.remove("bg-neutral-950");
+    logo?.classList.add("opacity-0");
+  } else if (scrollY > 0) {
+    header?.classList.add("bg-neutral-950");
+    logo?.classList.remove("opacity-0");
+  }
+});
